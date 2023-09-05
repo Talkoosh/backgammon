f87318852699db58c0e64b9f8106dc8f7285e3e9
TalSB <artishok.tal@gmail.com>
2023-09-06 02:08:15 +0300

    moved triangle to seperate comoponent, created checkers and put them in default starting position

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,9 +1,9 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import styles from './board.module.css';
+import { Triangle } from '../Triangle';
+
 export const Board = () => {
     const [boardData, setBoardData] = useState(getInitialBoardData());
-    const triangleHeight = Math.floor(window.innerHeight / 3);
-    console.log('🚀 ~ file: index.jsx:6 ~ Board ~ triangleHeight:', triangleHeight);
 
     return (
         <div className={styles.container}>
@@ -12,11 +12,7 @@ export const Board = () => {
                 <div className={styles.row} key={Math.random(0, 200)}>
                     {row.map((triangle, triangleIndex) => (
                         <>
-                            <span
-                                style={rowIndex === 0 ? { borderTopWidth: triangleHeight } : { borderBottomWidth: triangleHeight }}
-                                key={Math.random(0, 200)}
-                                className={`${rowIndex === 0 ? styles.triangleDown : styles.triangleUp} ${styles.triangle}`}
-                            ></span>
+                            <Triangle triangle={triangle} rowIndex={rowIndex}></Triangle>
                             {triangleIndex === 5 ? <span className={styles.empty}></span> : ''}
                         </>
                     ))}
@@ -27,11 +23,9 @@ export const Board = () => {
 };
 
 const getInitialBoardData = () => {
-    const initialData = [[], []];
-    for (let i = 0; i < 2; i++) {
-        for (let k = 0; k < 12; k++) {
-            initialData[i][k] = null;
-        }
-    }
+    const initialData = [
+        [{ type: 'white', amount: 5 }, {}, {}, {}, { type: 'black', amount: 3 }, {}, { type: 'black', amount: 5 }, {}, {}, {}, {}, { type: 'white', amount: 2 }],
+        [{ type: 'black', amount: 5 }, {}, {}, {}, { type: 'white', amount: 3 }, {}, { type: 'white', amount: 5 }, {}, {}, {}, {}, { type: 'black', amount: 2 }],
+    ];
     return initialData;
 };
---
+++ src/components/Triangle/index.jsx
@@ -0,0 +1,18 @@
+import styles from './triangle.module.css';
+
+export const Triangle = ({ rowIndex, triangle }) => {
+    const triangleHeight = Math.floor(window.innerHeight / 3);
+    console.log(triangle);
+
+    return (
+        <div style={rowIndex === 0 ? { borderTopWidth: triangleHeight } : { borderBottomWidth: triangleHeight }} className={`${rowIndex === 0 ? styles.triangleDown : styles.triangleUp} ${styles.triangle}`}>
+            <div className={rowIndex === 0 ? styles.checkersContainerTop : styles.checkersContainerBottom}>
+                {Array(triangle?.amount || 0)
+                    .fill(0)
+                    .map(() => (
+                        <div className={styles.checker} style={{ backgroundColor: triangle.type }}></div>
+                    ))}
+            </div>
+        </div>
+    );
+};
